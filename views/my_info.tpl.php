f769aa892c68ec7e2a0afd62b96351298c11fbf9
menthol <nath@noizers.com>
2012-10-07 09:50:08 +0200

    Biger imputs.

--- views/my_info.tpl.php
+++ views/my_info.tpl.php
@@ -3,20 +3,20 @@
   <div class="control-group">
     <label class="control-label" for="inputNom">Nom</label>
     <div class="controls">
-      <input type="text" id="inputNom" placeholder="Nom" name="name" value="<?php echo $user->name; ?>">
+      <input type="text" id="inputNom" placeholder="Nom" name="name" value="<?php echo $user->name; ?>" class="input-xxlarge">
     </div>
   </div>
   <div class="control-group">
     <label class="control-label" for="inputTel">Tel</label>
     <div class="controls">
-      <input type="text" id="inputTel" placeholder="Tel" name="tel" value="<?php echo $user->tel; ?>">
+      <input type="text" id="inputTel" placeholder="Tel" name="tel" value="<?php echo $user->tel; ?>" class="input-xxlarge">
       <small>Pour recevoir l’invitation par sms</small>
     </div>
   </div>
   <div class="control-group">
     <label class="control-label" for="inputRemarques">Remarques</label>
     <div class="controls">
-      <textarea id="inputRemarques" placeholder="Remarques" name="more" rows="10"><?php echo $user->more; ?></textarea><br />
+      <textarea id="inputRemarques" placeholder="Remarques" name="more" rows="10" class="input-xxlarge"><?php echo $user->more; ?></textarea><br />
        <small>Indiquez si vous venez à plusieurs, vos limitations alimentaires, etc.</small>
     </div>
   </div>
