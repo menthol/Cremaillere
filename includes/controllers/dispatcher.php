b8432a86a85b499937fa13cfb23530b78af1df71
menthol <nath@noizers.com>
2012-10-07 18:43:08 +0200

    Is current user is admin, the digicode must redirect to the edition form.

--- includes/controllers/dispatcher.php
+++ includes/controllers/dispatcher.php
@@ -2,6 +2,10 @@
 
 function controller_dispatcher($args) {
   if (user() && user()->is_admin) {
+    $quest = model('guest')->load(array('hash' => $args[0]));
+    if ($quest->id) {
+      redirect('my_info/' . $quest->id);
+    }
     redirect('my_guests');
   }
   redirect('login/' . $args[0]);
